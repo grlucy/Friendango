ea3a2512be51b236a05b9c9790f984aefcbf9037
MaryEtak <marykatewhite@gmail.com>
2020-02-19 10:53:44 -0500

    schema adjustments

--- db/seed.sql
+++ db/seed.sql
@@ -1,1 +1,1 @@
-INSERT INTO `users` (`username `, `email`, `password`) VALUES (`maryetak`, `marykatewhite@gmail.com`, `Fr33dum!`, );
+INSERT INTO `users` (`username `, `email`, `password`) VALUES (`maryetak`, `marykatewhite@gmail.com`, `Fr33dum!` );
