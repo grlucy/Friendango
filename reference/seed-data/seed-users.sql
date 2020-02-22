6c7dc551fc06551d10e9f9578859bccbe1cf6ada
Ben F <56423163+bferriman@users.noreply.github.com>
2020-02-22 14:52:30 -0500

    adding files with seed data for users and reviews

---
+++ reference/seed-data/seed-users.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (username, email, password, createdAt, updatedAt)
+VALUES ("jelly_badger", "jbadger12123878748@aol.com", "jeifIUe278Fkel", "2020-01-04 12:01:22", "2020-01-04 12:01:22"),
+("DankeyKang", "dkdkdkdkdkdk12312123@gmail.com", "ijoisef7672fhJ", "2020-01-12 07:08:22", "2020-01-12 07:08:22"),
+("TheCriticalestCritic", "ihateeverythingthemost@hotmail.com", "monkey", "2019-12-28 03:12:59", "2019-12-28 03:12:59"),
+("Bored-Boris", "boris@boristown.net", "123racecar321", "2019-12-16 18:51:18", "2019-12-16 18:51:18");
