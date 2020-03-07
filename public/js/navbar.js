649694e0f6281a41ffc37aaa9f16de6b171c9fc7
Gina Lucy <53538316+grlucy@users.noreply.github.com>
2020-03-07 16:27:28 -0500

    added .trim() to search bar validation

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
   const searchType = $(".dropdown-item");
   searchType.on("click", function(e) {
-    const searchValue = $("#searchValue").val();
+    const searchValue = $("#searchValue")
+      .val()
+      .trim();
     e.preventDefault();
-    if(searchValue !== "") {
+    if (searchValue !== "") {
       if (e.target.text === "Users") {
         window.location.href = `/users/${searchValue}`;
       } else if (e.target.text === "Movies") {
         window.location.href = `/api/movies/${searchValue}`;
-      }  
+      }
     }
   });
   const searchForm = $("#search-form");
