2b749ba757d4b9e03b0c766e4825792d43d32703
Gina Lucy <53538316+grlucy@users.noreply.github.com>
2020-03-07 17:48:20 -0500

    added login redirect to navbar search bar

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,18 +1,31 @@
 $(document).ready(function() {
+  // Check to see whether user is logged in
+  let loggedIn = false;
+  $.get("/api/user_data").then(function(data) {
+    if (data.username) {
+      loggedIn = true;
+    }
+  });
+
   const searchType = $(".dropdown-item");
   searchType.on("click", function(e) {
-    const searchValue = $("#searchValue")
-      .val()
-      .trim();
     e.preventDefault();
-    if (searchValue !== "") {
-      if (e.target.text === "Users") {
-        window.location.href = `/users/${searchValue}`;
-      } else if (e.target.text === "Movies") {
-        window.location.href = `/api/movies/${searchValue}`;
+    if (!loggedIn) {
+      window.location.href = `/login`;
+    } else {
+      const searchValue = $("#searchValue")
+        .val()
+        .trim();
+      if (searchValue !== "") {
+        if (e.target.text === "Users") {
+          window.location.href = `/users/${searchValue}`;
+        } else if (e.target.text === "Movies") {
+          window.location.href = `/api/movies/${searchValue}`;
+        }
       }
     }
   });
+
   const searchForm = $("#search-form");
   searchForm.on("submit", function(e) {
     e.preventDefault();
